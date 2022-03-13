b3b7a4a379eef9c2193848b2f13cf1d11eb60290
Prabhakar Mishra <mishrapra372000@gmail.com>
2022-03-13 13:12:36 +0530

    initial commit

---
+++ src/test/java/com/prabhu/springbootpractice/SpringBootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prabhu.springbootpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
